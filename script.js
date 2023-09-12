2fa3ab2b303dfbde362da2efc6aea0682026e700
Michaela <botka@email.cz>
2023-09-12 10:12:46 +0200

    Miniprojekt Scrollovací tlačítko - html, css, script

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-window.addEventListener("scroll", function(){
+window.addEventListener("scroll", function () {
     let scrolled = window.scrollY
     let scrollable = document.documentElement.scrollHeight - window.innerHeight
-    if(Math.ceil(scrolled) >300){
+    if (Math.ceil(scrolled) > 300) {
         topPage = document.querySelector(".top-page")
         topPage.style.display = "block"
 
-        topPage.addEventListener("click", function(){
+        topPage.addEventListener("click", function () {
             topPage.style.display = "none"
         })
     }
